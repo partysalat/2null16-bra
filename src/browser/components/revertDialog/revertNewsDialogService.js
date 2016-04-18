d6bf26d43b30e3a73bef4636c9b25285d8c2a314
Ben Lenser <ben.lenser@immobilienscout24.de>
2016-04-18 14:19:29 +0200

    add revert news

--- src/browser/components/revertDialog/revertNewsDialogService.js
+++ src/browser/components/revertDialog/revertNewsDialogService.js
@@ -1,7 +1,7 @@
 'use strict';
-module.exports = function ($mdDialog,News) {
-  function openModalForRevert(ev){
-    new News().$get().then(function(news){
+module.exports = function ($mdDialog, News) {
+  function openModalForRevert(ev) {
+    new News().$get().then(function (news) {
       return $mdDialog.show({
         controller: require("./revertNewsController"),
         templateUrl: 'revertNews.html',
@@ -9,15 +9,14 @@ module.exports = function ($mdDialog,News) {
         targetEvent: ev,
         locals: {news: news.news},
         clickOutsideToClose: true
-      })  
-    })
-    
-      
+      });
+    });
+
 
   }
 
   return {
-    openModalForRevert:openModalForRevert
+    openModalForRevert: openModalForRevert
   };
 };
 
