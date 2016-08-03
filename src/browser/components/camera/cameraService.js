be1e52dd2929beccf892f93417b0126ece354bca
Ben Lenser <ben.lenser@immobilienscout24.de>
2016-08-03 21:35:23 +0200

    add toast if image could not taken

--- src/browser/components/camera/cameraService.js
+++ src/browser/components/camera/cameraService.js
@@ -1,11 +1,18 @@
 'use strict';
-module.exports = function (Photo) {
+module.exports = function (Photo, $mdToast) {
   return {
     takePhoto: function () {
-      return new Photo().$save();
+      return new Photo().$save().catch(function () {
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent("Fehler bei Bild Aufnahme! Überprüfe Verbindung oder Batterie der Kamera!")
+            .position("top center")
+            .hideDelay(3000)
+        );
+      });
     }
   };
 };
 
 
-module.exports.$inject = ["Photo"];
+module.exports.$inject = ["Photo", "$mdToast"];
