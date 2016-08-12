fd143597a08d1d9e8c8c54317626dbb19a50850f
Ben Lenser <ben.lenser@immobilienscout24.de>
2016-08-12 18:12:57 +0200

    feat: show toast only, when status 500

--- src/browser/components/camera/cameraService.js
+++ src/browser/components/camera/cameraService.js
@@ -2,13 +2,15 @@
 module.exports = function (Photo, $mdToast) {
   return {
     takePhoto: function () {
-      return new Photo().$save().catch(function () {
-        $mdToast.show(
-          $mdToast.simple()
-            .textContent("Fehler bei Bild Aufnahme! Überprüfe Verbindung oder Batterie der Kamera!")
-            .position("top center")
-            .hideDelay(3000)
-        );
+      return new Photo().$save().catch(function (err) {
+        if(err.status >= 500){
+          $mdToast.show(
+            $mdToast.simple()
+              .textContent("Fehler bei Bild Aufnahme! Überprüfe Verbindung oder Batterie der Kamera!")
+              .position("top center")
+              .hideDelay(3000)
+          );
+        }
       });
     }
   };
