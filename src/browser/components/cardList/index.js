13ceba2a031575c44038711e6f342a5cfe323b5a
Ben Lenser <ben.lenser@immobilienscout24.de>
2017-02-19 15:15:24 +0100

    add unique filter to prevent list from containing duplicate entries

--- src/browser/components/cardList/index.js
+++ src/browser/components/cardList/index.js
@@ -1,6 +1,7 @@
 'use strict';
 angular.module("cardlist", [require("./../lightbox")])
 
-  .component("cardList", require("./cardListComponent"));
+  .component("cardList", require("./cardListComponent"))
+  .filter("unique", require("./uniqueFilter"));
 
 module.exports = "cardlist";
---
+++ src/browser/components/cardList/uniqueFilter.js
@@ -0,0 +1,44 @@
+/**
+ * Taken from https://github.com/angular-ui/angular-ui-OLDREPO/blob/master/modules/filters/unique/unique.js
+ * @returns {Function}
+ */
+'use strict';
+
+module.exports =  function () {
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var newItems = [];
+
+      var extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  };
+};
